097015afa41e3bc967ebd63f489d2251515ed564
CodeCloud04 <148639964+CodeCloud04@users.noreply.github.com>
2025-06-25 15:35:24 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from .NET Core Web API running in Docker!");
+app.MapGet("/", () => "✅ Hello from fixed .NET 6 app running in Docker!");
 
 app.Run();
